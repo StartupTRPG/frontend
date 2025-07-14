629c6d842eb8b82788874d28b513381aa18c3768
joominjae <raipier8818@gmail.com>
2025-07-14 19:13:57 +0900

    allowedhost 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-}) 
+  server: {
+    host: '0.0.0.0', 
+    port: 5173,
+    allowedHosts: [
+      'startuptrpg.duckdns.org',
+      'startuptrpg-api.duckdns.org',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
+})
